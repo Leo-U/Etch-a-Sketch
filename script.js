cb8951bea07652cc3c17d3a9bb09be3afb38ecd3
Leo J <joelhoward2@gmail.com>
2021-01-09 11:49:16 -0500

    make separate stylesheet

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-//styling:
-
-const gridContainer = document.querySelector('#gridContainer');
-gridContainer.style.height = '320px';
-gridContainer.style.width = '320px';
-gridContainer.style.backgroundColor = 'black';
-gridContainer.style.borderRadius = '2px'
-containerAll.style.width = '320px';
-containerAll.style.margin = '0 auto';
-containerAll.style.marginTop = '100px';
-formContainer.style.display = 'flex';
-formContainer.style.alignItems = 'center';
-formContainer.style.justifyContent = 'space-between';
-formContainer.style.width = '280px';
-formContainer.style.marginTop = '10px';
-formContainer.style.marginLeft = '17px';
-textField.style.width = '40px';
-textField.style.height = '34px';
-textField.style.border = '1.75px solid black';
-textField.style.textAlign = 'center';
-textField.style.fontSize = '19px';
-textField.style.marginLeft = '-20px';
-valueTooHigh.style.marginLeft = '65px';
-submitButton.style.height = '40px';
-submitButton.style.width = '80px';
-
 //functions:
-
-let userInput = 16;
-textField.value = 16;
-let gridContainerSize = parseInt(gridContainer.style.width.slice(0,3));
 
 const gridClear = () => {
     while (gridContainer.firstChild) {
@@ -36,14 +6,21 @@ const gridClear = () => {
     };
 };
 
+const gridContainer = document.querySelector('#gridContainer');
+gridContainer.style.height = '320px';
+gridContainer.style.width = '320px';
+let gridContainerSize = parseInt(gridContainer.style.width.slice(0,3));
+let userInput = 16;
+
 const gridInit = () => {
     gridClear();
+    let gridSquare;
     for (i=0; i<userInput**2; i++) {
-        let gridSquare;
+
         gridSquare = document.createElement('div');
         gridSquare.className = "gridSquares";
         gridSquare.style.float = 'right';
-        document.getElementById('gridContainer').appendChild(gridSquare);
+        gridContainer.appendChild(gridSquare);
         gridSquare.style.height = (gridContainerSize / userInput) + 'px';
         gridSquare.style.width = (gridContainerSize / userInput) + 'px';
     };
@@ -56,6 +33,8 @@ const gridInit = () => {
 };
 
 gridInit();
+
+textField.value = 16;
 
 const inputValue = () => {
     if (textField.value <= 100) {
@@ -71,18 +50,18 @@ const inputValue = () => {
 };
 
 const buttonToInverted = () => {
-    submitButton.style.background = 'url(images/pix-submit';
+    submitButton.style.background = 'url(images/pix-submit.png';
     submitButton.style.backgroundRepeat = 'no-repeat';
     submitButton.style.backgroundPosition = 'center';
     submitButton.style.backgroundColor = 'limegreen';
-}
+};
 
 const buttonToDefault = () => {
     submitButton.style.background = 'url(images/pix-submit-2.png';
     submitButton.style.backgroundRepeat = 'no-repeat';
     submitButton.style.backgroundPosition = 'center';
     submitButton.style.backgroundColor = 'black';
-}
+};
 
 // event listeners:
 
